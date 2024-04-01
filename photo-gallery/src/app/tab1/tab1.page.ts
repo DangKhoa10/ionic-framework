dfcb30792fc816deadfca2d1ae7b85c94871ca8a
DangKhoa <vodangkhoa835@gmail.com>
2024-04-01 14:48:18 +0700

    init commit

---
+++ photo-gallery/src/app/tab1/tab1.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController) {}
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+}
